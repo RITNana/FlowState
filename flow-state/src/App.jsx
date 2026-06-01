69d10513f0ed801411581553eec9c0b0c9f6c18d
Nana Sarpong <ns8363@g.rit.edu>
2026-06-01 16:15:52 -0400

    new project

---
+++ flow-state/src/App.jsx
@@ -0,0 +1,43 @@
+const handleTaskButton = () => {
+
+}
+
+const handleThemeButton = () => {
+  
+}
+
+function App() {
+  return (
+    <>
+    <div className="mainFunctionality">
+      <h1 className="title is-1 pl-4 pt-4">FlowState</h1>
+      <p className="subtitle pl-4 pt-4"> A student's flowchart for tasks</p>
+      <div className="buttons  mt-4 ml-4">
+        <button className="button is-primary">Create Task</button>
+        <button className="button is-warning">
+          Change Theme Mode
+        </button>
+        <button className ="button is-success">
+          Read Docs
+        </button>
+      </div>
+
+
+    </div>
+
+    <div className="content">
+
+    </div>
+
+    </>
+
+
+
+
+
+
+  )
+
+}
+
+export default App
