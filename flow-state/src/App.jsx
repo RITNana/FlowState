158c421a6c7574364db94af1746ba81c79c68427
Nana Sarpong <ns8363@g.rit.edu>
2026-06-14 21:02:57 -0400

    implemented state for button click

--- flow-state/src/App.jsx
+++ flow-state/src/App.jsx
@@ -1,43 +1,58 @@
-const handleTaskButton = () => {
+import { use, useState } from "react"
 
+function TaskButton({text, handleClick}) {
+ return <button className = "button is-primary" onClick={handleClick}>{text}</button>
 }
 
-const handleThemeButton = () => {
-  
-}
-
-function App() {
+function TaskModal({handleClose}) {
   return (
-    <>
-    <div className="mainFunctionality">
-      <h1 className="title is-1 pl-4 pt-4">FlowState</h1>
-      <p className="subtitle pl-4 pt-4"> A student's flowchart for tasks</p>
-      <div className="buttons  mt-4 ml-4">
-        <button className="button is-primary">Create Task</button>
-        <button className="button is-warning">
-          Change Theme Mode
-        </button>
-        <button className ="button is-success">
-          Read Docs
-        </button>
+
+    <div className="modal is-active">
+      <div className="modal-background"></div>
+      <div className="modal-content">
+        <input  className="input is-large" type="text" placeholder="Text input" />
       </div>
-
-
+      <button className="modal-close is-large" aria-label="close" onClick={handleClose}></button>
     </div>
-
-    <div className="content">
-
-    </div>
-
-    </>
-
-
-
-
-
 
   )
+}
 
+
+function App() {
+
+
+  const [showModal, setShowModal] = useState(false)
+  
+  const handleShowModal = () => {
+    setShowModal(true)
+  }
+
+  const closeModal = () => {
+    setShowModal(false)
+  }
+
+ 
+ 
+
+  return (
+    <>
+         {showModal && <TaskModal handleClose={closeModal}/>}
+      <div className="mainFunctionality">
+        <h1 className="title is-1 pl-4 pt-4">FlowState</h1>
+        <p className="subtitle pl-4 pt-4"> A student's flowchart for tasks</p>
+        <div className="buttons  mt-4 ml-4">
+        <TaskButton text="Create Task" handleClick={handleShowModal}/>
+
+          <button className="button is-warning">Change Mode</button>
+          <button className="button is-success">
+            Read Docs
+          </button>
+        </div>
+     
+      </div>
+    </>
+  )
 }
 
 export default App
