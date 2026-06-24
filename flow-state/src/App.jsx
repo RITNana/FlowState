b41d6582fef209a2672607dd60598ae6c7db7505
Nana Sarpong <ns8363@g.rit.edu>
2026-06-24 15:15:16 -0400

    Created interactivity for creating a task

--- flow-state/src/App.jsx
+++ flow-state/src/App.jsx
@@ -1,140 +1,202 @@
-import { use, useState } from "react"
+import { use, useState } from "react";
 
 function TaskButton({ text, handleClick }) {
-  return <button className="button is-primary" onClick={handleClick}>{text}</button>
+  return (
+    <button className="button is-primary" onClick={handleClick}>
+      {text}
+    </button>
+  );
 }
 
-function TaskModal({ name, value, description, clock, handleClose, handleSubmit }) {
-
-    let priorities = ["Urgent", "High", "Medium", "Low"]
-    let times = ["30:00", "60:00", "75:00", "90:00", "120:00"]
-
-
+function TaskModal({
+  value, 
+  name, // task name
+  description, // task description
+  handleClose,
+  handleSubmit,
+  onNameChange,
+  onDescripChange,
+  onPriorityChange,
+  onTimeChange,
+}) {
+  let priorities = ["Urgent", "High", "Medium", "Low"];
+  let times = ["30:00", "60:00", "75:00", "90:00", "120:00"];
 
   return (
-
     <>
       <div className="taskSection">
-        <label className="label ml-4 " >Name</label>
-        <input className="input ml-4" type="text" placeholder="Name of Task" value={value}/>
+        <label className="label ml-4 ">Name</label>
+        <input
+          className="input ml-4"
+          type="text"
+          placeholder="Name of Task"
+          value={value}
+          onChange={onNameChange}
+        />
         <label className="label ml-4">Description</label>
-        <input className="input ml-4" type="text" placeholder="Description of Task" value={value}/>
+        <input
+          className="input ml-4"
+          type="text"
+          placeholder="Description of Task"
+          value={value}
+          onChange={onDescripChange}
+        />
         <label className="label ml-4">Priority</label>
         <div className="select ml-4" id="priorities">
-        <select>
-          <option>Priority</option>
-          {priorities.map((priority, index) => {
-            return <option key={index}>{priority}</option>
-          })}
-        </select>
+          <select onChange={onPriorityChange}>
+            <option>Priority</option>
+            {priorities.map((priority, index) => {
+              return <option key={index}>{priority}</option>;
+            })}
+          </select>
         </div>
-          <label className="label ml-4">Clock</label>
-         <div className="select ml-4" id="timeSelect">
-        <select>
-          <option>Select a Time</option>
-          {times.map((time, index) => {
-            return <option key={index}>{time}</option>
-          })}
-        </select>
+        <label className="label ml-4">Clock</label>
+        <div className="select ml-4" id="timeSelect">
+          <select onChange={onTimeChange}>
+            <option>Select a Time</option>
+            {times.map((time, index) => {
+              return <option key={index}>{time}</option>;
+            })}
+          </select>
         </div>
       </div>
 
-      <button className="button is-success ml-4 mt-4">Save</button>
-      <button className="button is-danger ml-4 mt-4" onClick={handleClose}>Cancel Task</button>
+      <button className="button is-success ml-4 mt-4" onClick={handleSubmit}>
+        Save
+      </button>
+      <button className="button is-danger ml-4 mt-4" onClick={handleClose}>
+        Cancel Task
+      </button>
     </>
-
-  )
+  );
 }
 
-function TaskCard({taskName, taskValue, taskDescription, lengthSelected, priority}) {
+function TaskCard({
+  taskName,
+  taskValue,
+  taskDescription,
+  lengthSelected,
+  priority,
+}) {
   return (
-    <div class="card">
-  <header class="card-header">
-    <p class="card-header-title">{taskName}</p>
-    <button class="card-header-icon" aria-label="more options">
-      <span class="icon">
-        <i class="fas fa-angle-down" aria-hidden="true"></i>
-      </span>
-    </button>
-  </header>
-  <div class="card-content">
-    <div class="content">
-      {taskDescription}
-      {priority}
-      {lengthSelected}
-      <br />
-      
-    </div>
-  </div>
-  <footer class="card-footer">
-    <a href="#" class="card-footer-item">Save</a>
-    <a href="#" class="card-footer-item">Edit</a>
-    <a href="#" class="card-footer-item">Delete</a>
-  </footer>
-</div>
-  )
-}
-
-function DocModal(){
-  return (
-    <div class="modal is-active">
-  <div class="modal-background"></div>
-  <div class="modal-content">
     <div className="card">
+      <header className="card-header">
+        <p className="card-header-title">{taskName}</p>
+        <button className="card-header-icon" aria-label="more options">
+          <span className="icon">
+            <i className="fas fa-angle-down" aria-hidden="true"></i>
+          </span>
+        </button>
+      </header>
       <div className="card-content">
         <div className="content">
-          Say some
+          {taskDescription}
+          {priority}
+          {lengthSelected}
+          <br />
         </div>
       </div>
-
+      <footer className="card-footer">
+        {/* <a href="#" className="card-footer-item">Save</a> */}
+        <a href="#" className="card-footer-item">
+          Edit
+        </a>
+        <a href="#" className="card-footer-item">
+          Delete
+        </a>
+      </footer>
     </div>
-  </div>
-  <button class="modal-close is-large" aria-label="close"></button>
-</div>
-
-  )
+  );
 }
-  
 
+// function DocModal() {
+//   return (
+//     <div className="modal is-active">
+//       <div className="modal-background"></div>
+//       <div className="modal-content">
+//         <div className="card">
+//           <div className="card-content">
+//             <div className="content">
+//               Say some
+//             </div>
+//           </div>
 
+//         </div>
+//       </div>
+//       <button className="modal-close is-large" aria-label="close"></button>
+//     </div>
 
+//   )
+// }
 
 function App() {
+  // List of Tasks
+  let taskList = [];
 
+  const [createTask, setCreateTask] = useState(false);
+  const [task, setTask] = useState({name: "", description: "", priority: "", time: ""});
 
-  const [showModal, setShowModal] = useState(false)
+  const handleCreateTask = () => {
+    setCreateTask(true);
+  };
 
-  const handleShowModal = () => {
-    setShowModal(true)
+  const deleteCreateTask = () => {
+    setCreateTask(false);
+  };
+
+  const updateNameField = (e) => {
+    const newTask = {...task, name:e.target.value}
+    setTask(newTask)
   }
 
-  const closeModal = () => {
-    setShowModal(false)
+  const updateDescField = (e) => {
+    const newTask = {...task, description:e.target.value}
+    setTask(newTask)
   }
 
 
+  const updatePriorityField = (e) => {
+    const newTask = {...task, priority:e.target.value}
+    setTask(newTask)
+  }
+
+
+  const updateTimeField = (e) => {
+    const newTask = {...task, time:e.target.value}
+    setTask(newTask)
+  }
+
+  const submitTask = () => {
+    
+  }
 
 
   return (
     <>
-
       <div className="mainFunctionality">
         <h1 className="title is-1 pl-4 pt-4">FlowState</h1>
         <p className="subtitle pl-4 pt-4"> A student's flowchart for tasks</p>
         <div className="buttons  mt-4 ml-4">
-          <TaskButton text="Create Task" />
+          <TaskButton text="Create Task" handleClick={handleCreateTask} />
           <button className="button is-warning">Change Mode</button>
-          <button className="button is-success">
-            Read Docs
-          </button>
+          <button className="button is-success">Read Docs</button>
         </div>
-      <TaskModal />
-        {/* {showModal && <TaskModal handleClose={closeModal} />} */}
-      <TaskCard />
-      {/* <DocModal /> */}
+        {/* <TaskModal /> */}
+        {createTask
+         && 
+         <TaskModal 
+          onNameChange={updateNameField}
+          onDescripChange={updateDescField}
+          onPriorityChange={updatePriorityField}
+          onTimeChange={updateTimeField}
+         handleClose={deleteCreateTask} 
+
+        />}
+        {/* <TaskCard /> */}
+        {/* <DocModal /> */}
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
